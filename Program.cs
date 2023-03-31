c8fa2b9e9d91738508fa3df44e76e7054ba3a643
ETEC Adolpho Berezin <yasmin.freitas25@etec.sp.gov.br>
2023-03-31 20:37:41 -0300

    adiciona solução do execício nome completo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string nome, sobrenome;
+Console.Write("digite seu nome:.....:");
+nome= Console.ReadLine();
+
+Console.Write("digite seu sobrenome:");
+sobrenome= Console.ReadLine();
+
+Console.WriteLine($"nome completo: {nome} {sobrenome}");
+Console.WriteLine($"nome do catálogo: {sobrenome.ToUpper()}, {nome} ");
+
